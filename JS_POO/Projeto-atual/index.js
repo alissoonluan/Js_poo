d29269db52c89607eade417d938058f29095f493
alissoonluan <50708092+alissoonluan@users.noreply.github.com>
2021-04-13 14:44:46 -0300

    first commit

---
+++ JS_POO/Projeto-atual/index.js
@@ -0,0 +1,22 @@
+class Cliente {
+    nome;
+    cpf;
+    agencia;
+    saldo;
+    rg
+}
+
+const cliente1 = new Cliente()
+const cliente2 = new Cliente()
+
+
+cliente1.nome = "Ricardo"
+cliente1.cPF = 1112223309
+cliente1.agencia = 1001
+cliente1.saldo = 0
+cliente1.rg = 123456789
+
+cliente2.nome = "Alice"
+cliente2.cPF = 8882223309
+cliente2.agencia = 1001
+cliente2.saldo = 0
